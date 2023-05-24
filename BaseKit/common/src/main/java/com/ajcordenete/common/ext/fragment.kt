b0ec5a67c03b4c13182c2c542220a2a77f9005bf
ajcordenete <aljon.cordenete@gmail.com>
2023-05-24 17:58:44 +0800

    Add ext functions for activity, fragment and view

--- BaseKit/common/src/main/java/com/ajcordenete/common/ext/fragment.kt
+++ BaseKit/common/src/main/java/com/ajcordenete/common/ext/fragment.kt
@@ -8,6 +8,10 @@ import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
+import androidx.navigation.NavDirections
+import androidx.navigation.NavOptions
+import androidx.navigation.Navigator
+import androidx.navigation.fragment.findNavController
 
 fun Fragment.addMenuProvider(@MenuRes menuRes: Int, callback: (id: Int) -> Boolean) {
     val menuProvider = object : MenuProvider {
@@ -24,3 +28,25 @@ fun Fragment.addMenuProvider(@MenuRes menuRes: Int, callback: (id: Int) -> Boole
         Lifecycle.State.RESUMED
     )
 }
+
+fun Fragment.navigate(dest: NavDirections, navigatorExtras: Navigator.Extras? = null) {
+    if (navigatorExtras != null) {
+        findNavController()
+            .navigate(
+                dest,
+                navigatorExtras
+            )
+    } else {
+        findNavController().navigate(dest)
+    }
+}
+
+fun Fragment.navigate(dest: NavDirections, navOptions: NavOptions) {
+    findNavController()
+        .navigate(
+            dest,
+            navOptions
+        )
+}
+
+fun Fragment.navigateUp() = findNavController().navigateUp()
