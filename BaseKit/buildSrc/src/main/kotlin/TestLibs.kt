3dbde747eb89c3316f432fc2ea10f2e29182f0d1
ajcordenete <aljon.cordenete@gmail.com>
2023-06-09 16:48:33 +0800

    Update test libs dependencies

---
+++ BaseKit/buildSrc/src/main/kotlin/TestLibs.kt
@@ -0,0 +1,12 @@
+
+object TestLibs {
+    val androidXJUnit = "androidx.test.ext:junit:${Versions.androidXJunitVersion}"
+    val archCoreTesting = "androidx.arch.core:core-testing:${Versions.archCompTestVersion}"
+    val espresso = "androidx.test.espresso:espresso-core:${Versions.espressoVersion}"
+    val junit = "junit:junit:${Versions.junitVersion}"
+    val mockito = "org.mockito:mockito-inline:${Versions.mockitoVersion}"
+    val testRules = "androidx.test:rules:${Versions.testRulesVersion}"
+    val testRunner = "androidx.test:runner:${Versions.testRunnerVersion}"
+    val coroutinesTest = "org.jetbrains.kotlinx:kotlinx-coroutines-test:${Versions.coroutinesTestVersion}"
+    val turbine = "app.cash.turbine:turbine:${Versions.turbineVersion}"
+}
