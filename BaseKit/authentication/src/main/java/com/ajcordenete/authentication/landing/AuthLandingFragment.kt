bacfd339a672f326372dea25f5e30ac48c5127b9
ajcordenete <aljon.cordenete@gmail.com>
2023-07-18 17:42:34 +0800

    Implement auth landing page

--- BaseKit/authentication/src/main/java/com/ajcordenete/authentication/landing/AuthLandingFragment.kt
+++ BaseKit/authentication/src/main/java/com/ajcordenete/authentication/landing/AuthLandingFragment.kt
@@ -1,12 +1,40 @@
 package com.ajcordenete.authentication.landing
 
+import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.lifecycleScope
 import com.ajcordenete.authentication.R
 import com.ajcordenete.authentication.databinding.FragmentAuthLandingBinding
 import com.ajcordenete.core.base.BaseFragment
-import dagger.hilt.android.AndroidEntryPoint
+import com.ajcordenete.core.ext.navigate
+import com.ajcordenete.core.ext.ninjaTap
+import kotlinx.coroutines.flow.launchIn
 
-@AndroidEntryPoint
 class AuthLandingFragment: BaseFragment<FragmentAuthLandingBinding>() {
 
     override fun getLayoutId(): Int = R.layout.fragment_auth_landing
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setUpViews()
+    }
+
+    private fun setUpViews() {
+        binding
+            .btnLogin
+            .ninjaTap {
+                navigate(
+                    AuthLandingFragmentDirections
+                        .actionAuthLandingFragmentToLoginFragment()
+                )
+            }
+            .launchIn(lifecycleScope)
+
+        binding
+            .btnRegister
+            .ninjaTap {
+
+            }
+            .launchIn(lifecycleScope)
+    }
 }
